f60800618c913d43d58e0fe584158c7e5a128a47
Petrik <112963523+petrikpopov@users.noreply.github.com>
2024-09-17 19:55:59 +0300

    Add files via upload

---
+++ FirstProject_17/src/helloFirstProject/com/Program.java
@@ -0,0 +1,12 @@
+package helloFirstProject.com;
+
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+		System.out.println("hello naprimer!");
+		Scanner s;
+	}
+
+}
